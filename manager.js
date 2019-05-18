6cc469b83a962070a7877a440324542ef458c619
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-05-18 01:09:24 -0500

    minor refactoring, added mainloop function to make a little more readable

--- manager.js
+++ manager.js
@@ -45,207 +45,226 @@ inquirer.prompt([
     }
     else {
 
-        inquirer.prompt([
+        runMainLoop();
 
-            {
-                type: "list",
-                name: "action",
-                message: "Hello, how may I assist you today?",
-                choices: ["View products for sale", "View low inventory", "Add to inventory", "Add new product"]
-            }
-
-        ]).then(function (mgmt) {
-
-            if (mgmt.action === "View products for sale") {
-
-                viewProducts();
-
-            }
-            else if (mgmt.action === "View low inventory") {
-
-                viewLowInventory();
-
-            }
-            else if (mgmt.action === "Add to inventory") {
-
-                restockProduct(function (products) {
-
-                    const choicesDis = [];
-                    for (var i = 0; i < products.length; i++) {
-                        choicesDis.push(products[i].product);
-                    }
-
-                    inquirer.prompt([
-
-                        {
-                            type: "list",
-                            name: "product",
-                            message: "Which product are you replenishing today?",
-                            choices: choicesDis
-                        },
-
-                        {
-                            type: "input",
-                            name: "quantity",
-                            message: "How many units are you adding?",
-                            choices: choicesDis
-                        }
-
-                    ]).then(function (response) {
-
-                        console.log(response.product);
-
-                        searchStockByName(response.product, function (res) {
-
-                            console.log(`Adding ${response.quantity} ${response.product}(s)...\n`);
-                            const query = connection.query(
-                                "UPDATE products SET ? WHERE ?",
-                                [
-                                    {
-                                        stock: res.stock + parseInt(response.quantity)
-                                    },
-                                    {
-                                        product: response.product
-                                    }
-                                ],
-                                function (err, res) {
-                                    console.log(res.affectedRows + " products updated!\n");
-                                }
-                            );
-                        });
-                    });
-
-                });
-            }
-            else {
-
-                getDepartmentChoices(function (departments) {
-                    console.log("hello");
-
-                    const choicesDis = [];
-                    for (var i = 0; i < departments.length; i++) {
-                        choicesDis.push(departments[i].department);
-                    }
-
-                    inquirer.prompt([
-
-                        {
-                            type: "input",
-                            name: "product",
-                            message: "What product are we adding today?"
-                        },
-
-                        {
-                            type: "list",
-                            name: "department",
-                            message: "Which department will this product be?",
-                            choices: choicesDis
-                        },
-
-                        {
-                            type: "input",
-                            name: "price",
-                            message: "What will be the price of our new product?",
-                        },
-
-                        {
-                            type: "input",
-                            name: "stock",
-                            message: "How much inventory will we begin with?"
-                        }
-
-                    ]).then(function (res) {
-                        console.log("Inserting a new product...\n");
-                        const query = connection.query(
-                            "INSERT INTO products SET ?",
-                            {
-                                product: res.product,
-                                department: res.department,
-                                price: res.price,
-                                stock: res.stock,
-                                sale: 0,
-                                sale_price: null
-                            },
-                            function (err, res) {
-                                console.log(res.affectedRows + " product inserted!\n");
-                                // Call updateProduct AFTER the INSERT completes
-                            });
-                    });
-
-                });
-            }
-        });
     }
 });
 
 
-        function viewLowInventory(cutoff = 51) {
+function viewLowInventory(cutoff = 51) {
 
-            console.log("Finding low inventory...\n");
-            connection.query(`SELECT * FROM products WHERE stock < ${cutoff}`, function (err, res) {
-                if (err) throw err;
-                // Log all results of the SELECT statement
-                console.log(res);
-                connection.end();
-            });
+    console.log("\nFinding low inventory...\n");
+    connection.query(`SELECT * FROM products WHERE stock < ${cutoff}`, function (err, res) {
+        if (err) throw err;
+        console.log(`These items have less than ${cutoff} units in stock. Time to restock!`);
+        displayProducts(res);
 
+    });
+
+}
+
+function restockProduct(callback) {
+    connection.query(`SELECT product FROM products`, function (err, res) {
+        if (err) throw err;
+
+        callback(res);
+
+    });
+}
+
+function viewProducts() {
+    console.log("\nSelecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        displayProducts(res);
+        return (res);
+
+
+    });
+}
+
+function searchStockByName(name, callback) {
+    connection.query(`SELECT stock FROM products WHERE product = "${name}"`, function (err, res) {
+        if (err) throw err;
+        callback(res[0]);
+
+    });
+
+}
+
+//function addProduct(res) {
+//    console.log("Inserting a new product...\n");
+//    const query = connection.query(
+//        "INSERT INTO products SET ?",
+//        {
+//            product: res.product,
+//            department: res.department,
+//            price: res.price,
+//            stock: res.stock,
+//            sale: 0,
+//            sale_price: null
+//        },
+//        function (err, res) {
+//            console.log(res.affectedRows + " product inserted!\n");
+//             Call updateProduct AFTER the INSERT completes
+//        });
+
+//}
+
+
+function getDepartmentChoices(callback) {
+    connection.query(`SELECT DISTINCT department FROM products`, function (err, res) {
+        if (err) throw err;
+        callback(res);
+
+    });
+}
+
+
+function displayProducts(inv) {
+    console.log("----------------------------------------------------------------------------------");
+    for (var i = 0; i < inv.length; i++) {
+        console.log(`\nProduct Num: ${inv[i].id} | Product: ${inv[i].product} | Price: $${inv[i].price} | In Stock: ${inv[i].stock}`);
+    }
+
+    console.log("\n----------------------------------------------------------------------------------");
+
+}
+
+function runMainLoop() {
+
+
+    inquirer.prompt([
+
+        {
+            type: "list",
+            name: "action",
+            message: "Hello, how may I assist you today?",
+            choices: ["View products for sale", "View low inventory", "Add to inventory", "Add new product"]
         }
 
-        function restockProduct(callback) {
-            connection.query(`SELECT product FROM products`, function (err, res) {
-                if (err) throw err;
+    ]).then(function (mgmt) {
 
-                callback(res);
+        if (mgmt.action === "View products for sale") {
+
+            viewProducts();
+
+        }
+        else if (mgmt.action === "View low inventory") {
+
+            viewLowInventory();
+
+        }
+        else if (mgmt.action === "Add to inventory") {
+
+            restockProduct(function (products) {
+
+                const choicesDis = [];
+                for (var i = 0; i < products.length; i++) {
+                    choicesDis.push(products[i].product);
+                }
+
+                inquirer.prompt([
+
+                    {
+                        type: "list",
+                        name: "product",
+                        message: "Which product are you replenishing today?",
+                        choices: choicesDis
+                    },
+
+                    {
+                        type: "input",
+                        name: "quantity",
+                        message: "How many units are you adding?",
+                        choices: choicesDis
+                    }
+
+                ]).then(function (response) {
+
+                    console.log(response.product);
+
+                    searchStockByName(response.product, function (res) {
+
+                        console.log(`\nAdding ${response.quantity} ${response.product}(s)...\n`);
+                        const query = connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock: res.stock + parseInt(response.quantity)
+                                },
+                                {
+                                    product: response.product
+                                }
+                            ],
+                            function (err, res) {
+                                console.log(res.affectedRows + " products updated!\n");
+                            }
+                        );
+                    });
+                });
 
             });
         }
+        else {
 
-        function viewProducts() {
-            console.log("Selecting all products...\n");
-            connection.query("SELECT * FROM products", function (err, res) {
-                if (err) throw err;
-                displayProducts(res);
-                console.log(res);
-                return (res);
+            getDepartmentChoices(function (departments) {
 
-                connection.end();
+                const choicesDis = [];
+                for (var i = 0; i < departments.length; i++) {
+                    choicesDis.push(departments[i].department);
+                }
+
+                inquirer.prompt([
+
+                    {
+                        type: "input",
+                        name: "product",
+                        message: "What product are we adding today?"
+                    },
+
+                    {
+                        type: "list",
+                        name: "department",
+                        message: "Which department will this product be?",
+                        choices: choicesDis
+                    },
+
+                    {
+                        type: "input",
+                        name: "price",
+                        message: "What will be the price of our new product?",
+                    },
+
+                    {
+                        type: "input",
+                        name: "stock",
+                        message: "How much inventory will we begin with?"
+                    }
+
+                ]).then(function (res) {
+                    console.log("\nInserting a new product...\n");
+                    const query = connection.query(
+                        "INSERT INTO products SET ?",
+                        {
+                            product: res.product,
+                            department: res.department,
+                            price: res.price,
+                            stock: res.stock,
+                            sale: 0,
+                            sale_price: null
+                        },
+                        function (err, res) {
+                            console.log(res.affectedRows + " product inserted!\n");
+                            // Call updateProduct AFTER the INSERT completes
+                        });
+                });
+
             });
         }
-
-        function searchStockByName(name, callback) {
-            connection.query(`SELECT stock FROM products WHERE product = "${name}"`, function (err, res) {
-                if (err) throw err;
-                callback(res[0]);
-
-            });
-
-        }
-
-        //function addProduct(res) {
-        //    console.log("Inserting a new product...\n");
-        //    const query = connection.query(
-        //        "INSERT INTO products SET ?",
-        //        {
-        //            product: res.product,
-        //            department: res.department,
-        //            price: res.price,
-        //            stock: res.stock,
-        //            sale: 0,
-        //            sale_price: null
-        //        },
-        //        function (err, res) {
-        //            console.log(res.affectedRows + " product inserted!\n");
-        //             Call updateProduct AFTER the INSERT completes
-        //        });
-
-        //}
+    });
+}
 
 
-        function getDepartmentChoices(callback) {
-            connection.query(`SELECT DISTINCT department FROM products`, function (err, res) {
-                if (err) throw err;
-                console.log(res);
-                callback(res);
 
-            });
-        }
+
